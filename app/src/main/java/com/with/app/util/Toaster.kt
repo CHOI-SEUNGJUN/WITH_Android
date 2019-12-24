384222ebcabd171e7a3114ec4e1cc28132418bc0
june <brainyjun@gmail.com>
2019-12-24 15:49:05 +0900

    PrefManager, Toaster 확장 함수로 변경 + futura font 추가

--- app/src/main/java/com/with/app/util/Toaster.kt
+++ app/src/main/java/com/with/app/util/Toaster.kt
@@ -3,14 +3,16 @@ package com.with.app.util
 import android.content.Context
 import android.widget.Toast
 
-object Toaster {
+fun Context.toast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
 
-    fun toast(context : Context, msg : String) {
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
-    }
+fun Context.longToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+}
 
-    fun longToast(context : Context, msg : String) {
-        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
-    }
-
+fun Context.setStr(context: Context, key: String, value: String) {
+    val prefs = context.getSharedPreferences("", Context.MODE_PRIVATE)
+    val editor = prefs!!.edit()
+    editor.putString(key, value).apply()
 }
