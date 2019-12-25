b212787748c396a1d8c63c69f632e9b4cf5d779e
june <brainyjun@gmail.com>
2019-12-25 22:45:53 +0900

    Chat_List itemlist 뼈대 작성

--- app/src/main/java/com/with/app/ui/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/with/app/ui/chatlist/ChatListFragment.kt
@@ -1,6 +1,5 @@
 package com.with.app.ui.chatlist
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,16 +8,12 @@ import android.view.ViewGroup
 
 import com.with.app.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class ChatListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat_list, container, false)
     }
 
---
+++ app/src/main/java/com/with/app/ui/chatlist/recylcerview/ChatListViewHolder.kt
@@ -0,0 +1,8 @@
+package com.with.app.ui.chatlist.recylcerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatListViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+}
