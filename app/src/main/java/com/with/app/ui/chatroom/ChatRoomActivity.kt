c3ea3b3b53300955ba8c4b0a240c55651cb97624
june <brainyjun@gmail.com>
2019-12-25 17:17:05 +0900

    chat_room 기본 파일 생성(어뎁터, 뷰홀더, chatVO) status bar 투명색 설정

---
+++ app/src/main/java/com/with/app/ui/chatroom/ChatRoomActivity.kt
@@ -0,0 +1,13 @@
+package com.with.app.ui.chatroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.with.app.R
+
+class ChatRoomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+    }
+}
