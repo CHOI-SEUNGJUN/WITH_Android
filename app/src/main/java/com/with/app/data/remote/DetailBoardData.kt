8a88e6f81ad8c9ef52519eca63e22449f3124ba9
whgusdk98 <whgusdk1998@gmail.com>
2020-01-02 06:44:07 +0900

    postlist 통신 수정

--- app/src/main/java/com/with/app/data/remote/DetailBoardData.kt
+++ app/src/main/java/com/with/app/data/remote/DetailBoardData.kt
@@ -3,5 +3,5 @@ package com.with.app.data.remote
 data class ResponseDetailBoardData(
     val success : Boolean,
     val message : String,
-    val data : ResponseBoardObjectData
+    val data : List<ResponseBoardObjectData>
 )
--- app/src/main/java/com/with/app/ui/posting/PostingActivity.kt
+++ app/src/main/java/com/with/app/ui/posting/PostingActivity.kt
@@ -15,7 +15,6 @@ import com.with.app.ui.detailpost.DetailPostActivity
 import com.with.app.ui.home.HomeFragment
 import com.with.app.ui.region.ChangeRegionActivity
 import com.with.app.util.safeEnqueue
-import com.with.app.util.toast
 import kotlinx.android.synthetic.main.activity_posting.*
 import kotlinx.android.synthetic.main.activity_posting.btn_save
 import kotlinx.android.synthetic.main.activity_posting.switch_filter
@@ -23,12 +22,8 @@ import kotlinx.android.synthetic.main.date_picker.*
 import kotlinx.android.synthetic.main.date_picker.view.*
 import org.koin.android.ext.android.inject
 import java.text.SimpleDateFormat
-import com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.R
-import android.content.res.ColorStateList
+import com.with.app.util.toast
 
 
 class PostingActivity : AppCompatActivity() {
@@ -37,16 +32,17 @@ class PostingActivity : AppCompatActivity() {
     private val requestManager : RequestManager by inject()
     private var boardIdx = 0
     private lateinit var dialogView : View
+    private var mode = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.with.app.R.layout.activity_posting)
 
         dialogView = layoutInflater.inflate(com.with.app.R.layout.date_picker, null)
+        dialogView.btn_select_all.visibility = View.GONE
 
         //게시글 수정
         if(intent.getIntExtra("mode",0)==1) {
-            dialogView.btn_select_all.visibility = View.GONE
 
             boardIdx = intent.getIntExtra("boardIdx", 0)
             //수정에서 넘어왔을 때 게시글 수정 텍스트 변경, 삭제 버튼
@@ -61,58 +57,60 @@ class PostingActivity : AppCompatActivity() {
                 switch_filter.isChecked = false
                 switch_filter.toggle()
             }
-
             btn_delete.visibility = View.VISIBLE
             txt_category.text = "게시글 수정"
-            btn_save.setOnClickListener {
-                var regionCode = edt_region.text.toString()
-                var title = edt_title.text.toString()
-                var content = edt_content.text.toString()
-                var startDate = edt_date.text.split(" ~ ")[0]
-                var endDate = edt_date.text.split(" ~ ")[1]
-                var filter :Int
-                if(switch_filter.isChecked) filter = 1
-                else filter = -1
-                requestManager.requestBoardEdit(boardIdx,RequestBoardData(regionCode,title,content,startDate,endDate,filter))
-                    .safeEnqueue (
+            mode = 0
+        }
+
+        else {//게시글 작성
+            btn_delete.visibility = View.GONE
+            mode = 1
+        }
+
+        btn_save.setOnClickListener {
+            var regionCode = requestManager.regionManager.code
+            var title = edt_title.text.toString()
+            var content = edt_content.text.toString()
+            var startDate = edt_date.text.split(" ~ ")[0]
+            var endDate = edt_date.text.split(" ~ ")[1]
+            var filter: Int
+            if (switch_filter.isChecked){
+                filter = 1
+            }
+            else{
+                filter = -1
+            }
+            if (mode == 0) {
+                requestManager.requestBoardEdit(boardIdx, RequestBoardData(regionCode, title, content, startDate, endDate, filter))
+                    .safeEnqueue(
                         onSuccess = {
                             val intent = Intent()
-                            intent.putExtra("boardIdx",boardIdx)
-                            setResult(Activity.RESULT_OK,intent)
+                            intent.putExtra("boardIdx", boardIdx)
+                            setResult(Activity.RESULT_OK, intent)
+                            finish()
                         },
                         onError = {
                             Log.e("error", it.toString())
                         }
                     )
-
-            }//서버에게 수정저장 요청후 상세 게시글 액티비티로 전환하면서 boardIdx넘김
-        }
-
-        else {//게시글 작성
-            btn_delete.visibility = View.GONE
-            dialogView.btn_select_all.visibility = View.GONE
-
-            btn_save.setOnClickListener {
-                var regionCode = edt_region.text.toString()
-                var title = edt_title.text.toString()
-                var content = edt_content.text.toString()
-                var startDate = edt_date.text.split(" ~ ")[0]
-                var endDate = edt_date.text.split(" ~ ")[1]
-                var filter :Int
-                if(switch_filter.isChecked) filter = 1
-                else filter = -1
+            }
+            else {
                 requestManager.requestBoardWrite(RequestBoardData(regionCode,title,content,startDate,endDate,filter))
                     .safeEnqueue (
                         onSuccess = {
                             val intent = Intent(this,DetailPostActivity::class.java)
                             intent.putExtra("boardIdx",boardIdx)
                             startActivity(intent)
+                            finish()
                         },
                         onError = {
                             Log.e("error", it.toString())
+                        },
+                        onFailure = {
+                            Log.e("failure", it.toString())
                         }
                     )
-            }//서버에게 저장 요청
+            }
         }
 
         edt_region.setOnClickListener {
@@ -141,7 +139,7 @@ class PostingActivity : AppCompatActivity() {
                     val pattern = SimpleDateFormat("yyyy.MM.dd")
                     val diffs = pattern.parse(tempEnd).compareTo(pattern.parse(tempStart))
                     if (diffs == -1) {
-                        context.toast("마감일이 시작일보다 늦어야합니다.")
+                        toast("마감일이 시작일보다 늦어야합니다.")
                         return@setOnClickListener
                     }
                     edt_date.text = "${start_datepicker.year%100}.${start_datepicker.month+1}.${start_datepicker.dayOfMonth} " +
