fcb5981c48a71fb43dcdae7e1209fddcf75d9da9
yeonghyeonSeok <syh4834@naver.com>
2020-01-02 18:11:51 +0900

    completed eval

--- app/src/main/java/com/with/app/ui/chatlist/evaluation/EvaluateActivity.kt
+++ app/src/main/java/com/with/app/ui/chatlist/evaluation/EvaluateActivity.kt
@@ -1,6 +1,5 @@
 package com.with.app.ui.chatlist.evaluation
 
-import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,7 +7,7 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import com.with.app.R
-import com.with.app.ui.region.ChangeRegionActivity
+import com.with.app.util.toSpanned
 import kotlinx.android.synthetic.main.activity_evaluate.*
 
 class EvaluateActivity : AppCompatActivity() {
@@ -19,16 +18,13 @@ class EvaluateActivity : AppCompatActivity() {
 
         var eval_mate = ArrayList<String>()
         eval_mate.add("석영현")
-        eval_mate.add("안현준")
-        eval_mate.add("조현아")
-        eval_mate.add("최승준")
-
+        
 
         //tv_elevation_count.setText("$eval_count")
 
         val eval_count_total = eval_mate.size
         var name = eval_mate[0]
-        tv_elevation_intro.setText("$name" + "님과의\n동행은 어떠셨나요?")
+        tv_elevation_intro.text = "<b>${name}님</b>과의<br>동행은 어떠셨나요?".toSpanned()
         tv_elevation_count_total.setText("$eval_count_total")
 
         img_close_button.setOnClickListener {
@@ -101,7 +97,7 @@ class EvaluateActivity : AppCompatActivity() {
                 var count = eval_count + 1
                 name = eval_mate[next_mate]
                 var count_string = count.toString()
-                tv_elevation_intro.setText("$name" + "님과의\n동행은 어떠셨나요?")
+                tv_elevation_intro.text = "<b>${name}님</b>과의<br>동행은 어떠셨나요?".toSpanned()
                 btn_elevation_top.setText("좋았어요")
                 btn_elevation_bottom.setText("별로였어요")
                 tv_elevation_count.setText(count_string)
@@ -123,7 +119,7 @@ class EvaluateActivity : AppCompatActivity() {
                 var count = eval_count - 1
                 name = eval_mate[prev_mate]
                 var count_string = count.toString()
-                tv_elevation_intro.setText("$name" + "님과의\n동행은 어떠셨나요?")
+                tv_elevation_intro.text = "<b>${name}님</b>과의<br>동행은 어떠셨나요?".toSpanned()
                 btn_elevation_top.setText("좋았어요")
                 btn_elevation_bottom.setText("별로였어요")
                 tv_elevation_count.setText(count_string)
@@ -161,7 +157,7 @@ class EvaluateActivity : AppCompatActivity() {
                                 name = eval_mate[eval_count]
                                 var count = eval_count + 1
                                 var count_string = count.toString()
-                                tv_elevation_intro.setText("$name" + "님과의\n동행은 어떠셨나요?")
+                                tv_elevation_intro.text = "<b>${name}님</b>과의<br>동행은 어떠셨나요?".toSpanned()
                                 btn_elevation_top.setText("좋았어요")
                                 btn_elevation_bottom.setText("별로였어요")
                                 tv_elevation_count.setText(count_string)
@@ -193,7 +189,7 @@ class EvaluateActivity : AppCompatActivity() {
                         MotionEvent.ACTION_UP -> {
                             btn_elevation_top.setBackgroundColor(Color.parseColor("#4DFFFFFF"))
                             if (eval_count == eval_count_total) {
-                                tv_elevation_intro.setText("감사합니다\n앞으로도 W!TH해요 :)")
+                                tv_elevation_intro.text = "감사합니다\n앞으로도 W!TH해요 :)"
                                 btn_elevation_bottom.setText("위드하기")
                                 btn_elevation_top.visibility = View.GONE
                                 img_close_button.visibility = View.GONE
@@ -208,7 +204,8 @@ class EvaluateActivity : AppCompatActivity() {
                                 name = eval_mate[eval_count]
                                 var count = eval_count + 1
                                 var count_string = count.toString()
-                                tv_elevation_intro.setText("$name" + "님과의\n동행은 어떠셨나요?")
+
+                                tv_elevation_intro.text = "<b>${name}님</b>과의<br>동행은 어떠셨나요?".toSpanned()
                                 btn_elevation_top.setText("좋았어요")
                                 btn_elevation_bottom.setText("별로였어요")
                                 tv_elevation_count.setText(count_string)
